5654f2300cd03fb51b240901e66e6c8412ea9cd2
mohamedhamouda7897 <mohamed.hamouda7897@gmail.com>
2024-12-20 10:49:00 +0200

    app init

---
+++ android/app/src/main/kotlin/com/example/todo_c13_friday/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.todo_c13_friday
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screens/introduction_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class IntroductionScreen extends StatelessWidget {
+  static const String routeName = "IntroScreen";
+
+  const IntroductionScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(),
+    );
+  }
+}
